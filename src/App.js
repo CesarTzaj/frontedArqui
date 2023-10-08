5ae9346ea038e18b6de7d2486cad2392237735d6
Cesar Tzaj <cesar.tzaj@outlook.com>
2023-10-07 22:09:13 -0600

    se agrego git hub pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
    <React.Fragment>
     <h1 className='text-5xl text-center'>Cesar Tzaj</h1>
+    <h1 className='text-5xl text-center'>Arqui II </h1>
+    <h1 className='text-5xl text-center'>Tarea 10 </h1>
     <Main></Main>
    </React.Fragment>
   );
